b3e9ab12ba9b6626c48785def5aba31c0416987b
Xenomii <voltzxp@gmail.com>
2022-03-14 14:26:36 +0800

    edited md5_pin_crack.py

--- scripts/md5_pin_crack.py
+++ scripts/md5_pin_crack.py
@@ -1,7 +1,8 @@
 import hashlib
 
 for i in range(1000000):
-    pin = '5260452f' + '{:06d}'.format(i)
-    check = hashlib.md5(pin.encode())
+    pin = '{:06d}'.format(i)
+    plaintext = '5260452f' + pin
+    check = hashlib.md5(plaintext.encode())
     if str(check.hexdigest()) == '5cdacc3ed69889ee8390d44a5381e7b0':
         print(pin)
